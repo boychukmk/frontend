aaddabc6b92a808bbf983f7e747f0859e37fb5b6
Max Boichuk <boychuk.mk@gmail.com>
2023-11-20 23:30:48 +0200

    second js animation

--- js/main.js
+++ js/main.js
@@ -7,6 +7,29 @@ document.addEventListener('DOMContentLoaded', function() {
     });
   });
 
+document.addEventListener('DOMContentLoaded', function() {
+  const bannerHeader = document.querySelector('.banner-text');
+  const textToAdd = ' Вітаємо з прийдешніми святами! Щастя вам, здоровля, многая літа, будьте щасливі.                      ' +
+    'А тепер анекдот: Іде по місту конячка... Дивиться у магазині продають соломʼяні капелюхи. Думає : "зайду подивлюсь!".' +
+    ' Заходить дивиться, каже дайте мені один будь ласка. Продавець питає: "Вам великий?". А конячка йому :"Та ні, маленький - я на дієті".' +
+    ' Ірина Аркадіївна, як вам анекдот?)';
+  const characters = textToAdd.split('');
+   let index = 0;
+  function addText() {
+    bannerHeader.textContent += characters[index];
+    index++;
+
+    if (index < characters.length) {
+      setTimeout(addText, 50); // Затримка між символами
+    }
+  }
+  addText();
+});
+
+
+
+
+
 
 
 
