16619cd06bea9de16dd41efc102ccb2e46d94fd9
Max Boichuk <boychuk.mk@gmail.com>
2023-11-20 21:32:17 +0200

    first js animation

---
+++ js/main.js
@@ -0,0 +1,12 @@
+
+document.addEventListener('DOMContentLoaded', function() {
+  const responceTrigger = document.querySelector('.responce-trigger');
+  const responceBox = document.querySelector('.unit-responce');
+  responceTrigger.addEventListener('click', function() {
+    responceBox.classList.add('active');
+    });
+  });
+
+
+
+
